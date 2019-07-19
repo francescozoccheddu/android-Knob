b8922f8b6db330a16c6bd028ec8cc06ebc303b9c
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2019-07-19 16:13:47 +0200

    First commit

---
+++ knob/src/main/java/com/francescozoccheddu/knob/KnobView.kt
@@ -0,0 +1,20 @@
+package com.francescozoccheddu.knob
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class KnobView : View {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    )
+
+
+}
