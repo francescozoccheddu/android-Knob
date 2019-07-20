20613b3eabd77f2cb29983d5173dd789bc0e493e
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2019-07-20 18:47:16 +0200

    Update testbed

--- app/src/main/java/com/francescozoccheddu/knobtestbed/MainActivity.kt
+++ app/src/main/java/com/francescozoccheddu/knobtestbed/MainActivity.kt
@@ -1,53 +1,12 @@
 package com.francescozoccheddu.knobtestbed
 
+import android.app.Activity
 import android.os.Bundle
-import android.widget.SeekBar
-import androidx.appcompat.app.AppCompatActivity
-import com.francescozoccheddu.knob.KnobView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val knob = findViewById<KnobView>(R.id.knob)
-
-        findViewById<SeekBar>(R.id.sb_max).setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                knob.maxValue = p1.toFloat() + 100f
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-            }
-
-        })
-        findViewById<SeekBar>(R.id.sb_min).setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                knob.minValue = p1.toFloat()
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-            }
-
-        })
-        findViewById<SeekBar>(R.id.sb_val).setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                knob.value = p1.toFloat()
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-            }
-
-        })
     }
 }
